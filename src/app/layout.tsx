49f671fb8d2314897a076f34d23e4a9c3ceb2987
Alevas004 <aleinfocontent@gmail.com>
2025-07-02 05:55:20 -0500

    details

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,35 @@
-import type { Metadata } from 'next'
-import { SessionProvider } from 'next-auth/react'
-import { auth } from '@/auth'
-import Header from '@/components/commons/header'
-import './globals.css'
+import type { Metadata } from "next";
+import { SessionProvider } from "next-auth/react";
+import { auth } from "@/auth";
+import Header from "@/components/commons/header";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Clinica Veterinaria',
-  description: 'Aplicación para la gestión de una clínica veterinaria'
-}
+  title: "PET CENTER",
+  description: "The best place to take care of your pets",
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+  },
+};
 
 interface RootLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-export default async function RootLayout ({ children }: Readonly<RootLayoutProps>) {
-  const session = await auth()
+export default async function RootLayout({
+  children,
+}: Readonly<RootLayoutProps>) {
+  const session = await auth();
   return (
     <SessionProvider session={session}>
-      <html lang='es'>
-        <body className='flex min-h-screen flex-col'>
+      <html lang="es">
+        <body className="flex min-h-screen flex-col">
           <Header />
-          <main className='flex-1'>
-            {children}
-          </main>
+          <main className="flex-1">{children}</main>
         </body>
       </html>
     </SessionProvider>
-  )
+  );
 }
